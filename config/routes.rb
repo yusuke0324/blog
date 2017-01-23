afc9b81e6805f9e233bc7c2201e7fdf81303b420
yusuke0324 <take.yusuke@gmail.com>
2017-01-22 23:12:58 -0800

    make blog

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :articles do
+    resources :comments
+  end
+  root 'welcom#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
